771d31905fa1cd1b538432391791af61d8c4d64a
Sebastian Müller <sebastian@hazelcode.com>
2010-04-26 21:44:44 +0200

    Added init.php, README and SQLJoin.class.php

---
+++ init.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * SimpleObjects
+ **
+ * To use SimpleObjects please add the following lines to your code:
+ *
+ * <?php
+ * // Define absolute path to SimpleObjects
+ * define('PATH_SIMPLEOBJECTS', '/your/absolute/path/to/SimpleObjects/');
+ * require_once PATH_SIMPLEOBJECTS . 'init.php';
+ *
+ * @package SimpleObjects
+ * @author Sebastian Müller
+ * @version 0.1
+ * @link http://github.com/hazelcode/SimpleObjects
+ */
+
+require_once PATH_SIMPLEOBJECTS . 'SQLJoin.class.php'; 
+
+?>
